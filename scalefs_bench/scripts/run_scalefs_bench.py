aecb61536abc86bb20b8f8a81fbc889f22117867
Chenhao Ye <ye.chenhao.ych@gmail.com>
2021-09-13 14:30:21 -0600

    bug fix

--- scalefs_bench/scripts/run_scalefs_bench.py
+++ scalefs_bench/scripts/run_scalefs_bench.py
@@ -10,17 +10,27 @@ CFS_MAIN_BIN_NAME = os.environ['CFS_MAIN_BIN_NAME']
 # 1-based index
 coord_pin_core = 21
 
+
 def print_usage_and_exit(argv0):
-    print(f"Usage: {argv0} {{smallfile|largefile}} {{ufs|ext4}} <log_dir>")
+    print(
+        f"Usage: {argv0} {{smallfile|largefile}} {{ufs|ext4}} <log_dir> [--no-cleanup]"
+    )
     exit(1)
 
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 4 and len(sys.argv) != 5:
     print_usage_and_exit(sys.argv[0])
 
 bench = sys.argv[1]
 fs_type = sys.argv[2]
 log_dir = sys.argv[3]
+no_cleanup = False
+if len(sys.argv) == 5:
+    if sys.argv[4] == "--no-cleanup":
+        no_cleanup = True
+    else:
+        print(f"Unknown argument: {sys.argv[4]}")
+        print_usage_and_exit()
 num_cpu = 10
 
 if bench == "smallfile":
@@ -40,20 +50,21 @@ elif fs_type == "ext4":
 else:
     print_usage_and_exit(sys.argv[0])
 
-
 ready_fname = "/tmp/cfs_ready"
 exit_fname = "/tmp/cfs_exit"
 os.environ["READY_FILE_NAME"] = ready_fname
+
 
 def cleanup():
     subprocess.call(["killall", "largefile"])
     subprocess.call(["killall", "smallfile"])
     subprocess.call(["killall", "cfs_bench_coordinator"])
+    subprocess.call(["killall", "fsMain"])
     if is_ufs:
-        ret = subprocess.run([MKFS_SPDK_BIN, "mkfs"])
+        ret = subprocess.call([MKFS_SPDK_BIN, "mkfs"])
         assert ret == 0
     else:
-        ret = subprocess.run(f"rm -rf {exper_dir}/*", shell=True)
+        subprocess.call(f"rm -rf {exper_dir}/*", shell=True)
     ret = subprocess.call(["sync"])
     assert ret == 0
     with open("/proc/sys/vm/drop_caches", "w") as f:
@@ -68,11 +79,15 @@ def cleanup():
     subprocess.call(["fstrim", "-a", "-v"])
     time.sleep(1)
 
+
 def prep_config():
     with open("/tmp/fsp.conf", "w") as f:
-        f.write('splitPolicyNum = "5";\nserverCorePolicyNo = "5";\ndirtyFlushRatio = "0.9";\nraNumBlock = "16";\n')
+        f.write(
+            'splitPolicyNum = "5";\nserverCorePolicyNo = "5";\ndirtyFlushRatio = "0.9";\nraNumBlock = "16";\n'
+        )
     with open("/tmp/spdk.conf", "w") as f:
         f.write('dev_name = "spdkSSD";\ncore_mask = "0x2";\nshm_id = "9";\n')
+
 
 # Use same number of workers as apps
 def start_fsp(num_app, fsp_out):
@@ -108,57 +123,69 @@ def shutdown_fsp(fs_proc):
     if subprocess.run(["pkill", "fsMain"]).returncode == 0:
         print("WARN: Detect fsMain after shutdown; kill...")
 
+
 def start_coord(num_app):
-    return subprocess.Popen([COORD_PATH, "-n", str(num_app), "-c", str(coord_pin_core)])
+    return subprocess.Popen(
+        [COORD_PATH, "-n",
+         str(num_app), "-c",
+         str(coord_pin_core)])
+
 
 def run_smallfile(nc):
-    curr_log_dir = f"{log_dir}/small-create_ncpu-{nc}"
+    curr_log_dir = f"{log_dir}/smallfile_ncpu-{nc}"
     os.mkdir(curr_log_dir)
     if is_ufs:
-        # prep fsp.conf
         with open(f"{curr_log_dir}/fsp.log", "w") as f:
-            fs_proc = start_fsp(nc, "/tmp/fsp.conf", f)
-    p = subprocess.run(["build/smallfile", "--prep", exper_dir])
-    assert p.returncode == 0
+            fs_proc = start_fsp(nc + 1, f)
+    ret = subprocess.call(["build/smallfile", "--prep", exper_dir])
+    assert ret == 0
 
+    curr_log_subdir = f"{curr_log_dir}/create"
     coord_proc = start_coord(nc)
     bench_list = []
     for c in range(nc):
-        with open(f"{curr_log_dir}/core-{c}.log", "w") as f:
-            bench_list.append(
-                subprocess.Popen(["build/smallfile", "--bench", "-y", "3", "-c", str(c), exper_dir], stdout=f)
-            )
+        with open(f"{curr_log_subdir}/core-{c}.log", "w") as f:
+            p = subprocess.Popen([
+                "build/smallfile", "--bench", "-y", "3", "-c",
+                str(c), exper_dir
+            ],
+                                 stdout=f)
+            bench_list.append(p)
     ret = coord_proc.wait()
     assert ret == 0
     for b in bench_list:
-        ret = coord_proc.wait()
+        ret = b.wait()
         assert ret == 0
     if is_ufs:
         shutdown_fsp(fs_proc)
 
-def run_largefile(nc, workload):
-    assert workload == "create" or workload == "overwrite"
-    curr_log_dir = f"{log_dir}/large-{workload}_ncpu-{nc}"
+
+def run_largefile(nc):
+    curr_log_dir = f"{log_dir}/largefile_ncpu-{nc}"
     os.mkdir(curr_log_dir)
     if is_ufs:
-        # prep fsp.conf
         with open(f"{curr_log_dir}/fsp.log", "w") as f:
-            fs_proc = start_fsp(nc, "/tmp/fsp.conf", f)
-    p = subprocess.run(["build/largefile", "--prep", exper_dir])
-    assert p.returncode == 0
-
-    coord_proc = start_coord(nc)
-    bench_list = []
-    for c in range(nc):
-        with open(f"{curr_log_dir}/core-{c}.log", "w") as f:
-            bench_list.append(
-                subprocess.Popen(["build/largefile", "--bench", workload, "-c", str(c), exper_dir], stdout=f)
-            )
-    ret = coord_proc.wait()
+            fs_proc = start_fsp(2 * nc + 1, f)
+    ret = subprocess.call(["build/largefile", "--prep", exper_dir])
     assert ret == 0
-    for b in bench_list:
+
+    for workload in ["create", "overwrite"]:
+        curr_log_subdir = f"{curr_log_dir}/{workload}"
+        coord_proc = start_coord(nc)
+        bench_list = []
+        for c in range(nc):
+            with open(f"{curr_log_subdir}/core-{c}.log", "w") as f:
+                p = subprocess.Popen([
+                    "build/largefile", "--bench", workload, "-c",
+                    str(c), exper_dir
+                ],
+                                     stdout=f)
+                bench_list.append(p)
         ret = coord_proc.wait()
         assert ret == 0
+        for b in bench_list:
+            ret = b.wait()
+            assert ret == 0
     if is_ufs:
         shutdown_fsp(fs_proc)
 
@@ -172,4 +199,5 @@ for nc in range(1, num_cpu + 1):
     else:
         run_largefile(nc, "create")
         run_largefile(nc, "overwrite")
-cleanup()
+if not no_cleanup:
+    cleanup()
